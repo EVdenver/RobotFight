cd7faeeb2988147e763d14d1d784ea126acdf6ef
Vincent <vincent.marechal.pro@gmail.com>
2019-11-25 16:21:34 +0100

    Optimisation et commentaire

--- RobotFightJava/src/Boussole.java
+++ RobotFightJava/src/Boussole.java
@@ -1,25 +1,30 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 
+ * @author darkf
+ * La boussole du Robot lui permettant de se déplacer sur la table
+ */
 public class Boussole {
-	
+	//Les points cardinaux de la boussole
 	Map<String,Integer> card = new HashMap<>();
+	//La direction dans laquelle il regarde
 	int dir;
+	//La position du robot
 	Position pos;
 	
 	//Constructor
 	/**
 	 * @author darkf
 	 * Constructeur par défaut, initialise les 4 points cardinaux et établi la base du Robot comme étant la position 0
-	 * Et la direction à l'Ouest (Direction de la base ennemie)
+	 * Et la position par défaut du robot
 	 */
 	public Boussole() {
 		card.put("Nord",90);
 		card.put("Sud", 270);
 		card.put("Est", 0);
 		card.put("Ouest",180);
-		card.put("Base", 0);
-		dir = card.get("Ouest");
 		pos = new Position();
 	}
 	
@@ -27,15 +32,21 @@ public class Boussole {
 	 * @author darkf
 	 * @see Boussole.Boussole()
 	 * @param angle angle de base vers lequel le robot porte son regard lors de l'initialisation de la boussole
+	 * @param x position en abcisse du robot
+	 * @param y position en ordonnées du robot
 	 */
 	public Boussole(int angle, int x, int y) {
-		super();
+		this();
 		dir = angle;
 		pos.setX(x);
 		pos.setY(y);
 	}
 	
 	//Methods
+	/**
+	 * @author darkf
+	 * @return dir la direction dans laquelle regarde le robot
+	 */
 	public int getDir() {
 		return this.dir;
 	}
@@ -62,6 +73,11 @@ public class Boussole {
 		}else {
 			throw new IllegalArgumentException("La cardinalité voulu n'éxiste pas");
 		}
+	}
+	
+	public void setPosition(int x, int y) {
+		pos.setX(x);
+		pos.setY(y);
 	}
 }
 
--- RobotFightJava/src/Carte.java
+++ RobotFightJava/src/Carte.java
@@ -1,42 +1,49 @@
 
+/**
+ * 
+ * @author darkf
+ * 
+ */
 public class Carte {
-	private Base baseA;
-	private Base baseE;
+	private String baseA;
+	private String baseE;
 	private int nbPalet;
 	private Palet[] tabP = new Palet[9];
 	
 	
-	public Base getBaseA() {return baseA;}
+	public String getBaseA() {return baseA;}
 
-	public void setBaseA(Base baseA) {this.baseA = baseA;}
-
-	public Base getBaseE() {return baseE;}
-
-	public void setBaseE(Base baseE) {this.baseE = baseE;}
+	public String getBaseE() {return baseE;}
 
 	public int getNbPalet() {return nbPalet;}
-
-	public void setNbPalet(int nbPalet) {this.nbPalet = nbPalet;}
+	
+	public Palet[] getTabP() {return tabP;}
 
 	public Palet getTabP(int i) {return tabP[i];}
 
 	public void setTabP(Palet tabP,int i) {this.tabP[i] = tabP;}
 
 	public Carte() {
-		baseA = new Base();
-		baseE = new Base();
-		setNbPalet(9);
+		baseA = new String();
+		baseE = new String();
+		nbPalet = 9;
 		for (int i = 0; i < tabP.length; i++) {
 			setTabP(new Palet(),i);
 		}
 	}
 	
-	public Carte(Position[] posP,Position posA, Position posE, String dirA,String dirE) {
-		baseA = new Base(posA,dirA);
-		baseE = new Base(posE,dirE);
-		setNbPalet(9);
-		for (int i = 0; i < posP.length; i++) {
-			setTabP(new Palet(posP[1]),i);
+	public Carte(String dirA,String dirE) {
+		baseA = dirA;
+		baseE = dirE;
+		nbPalet = 9;
+		int i = 0;
+		while(i != 9) {
+			for (int x = 90; x < 211;x = x+60 ) {
+				for (int y = 50; y < 151; y = y+50) {
+					setTabP(new Palet(new Position(x,y)),i);
+					i++;
+				}
+			}
 		}
 	}
 }
